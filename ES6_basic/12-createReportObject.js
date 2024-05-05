d4899d9a106bd358989902fadf5cf7ff9065d4d4
kvang2 <kevinvaj88@hotmail.com>
2024-05-05 16:33:45 -0500

    fix stuff on file 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,7 @@
 export default function createReportObject(employeesList) {
   const allEmployees = {};
   for (const department in employeesList) {
-    if (Object.hasOwn(employeesList, department)) {
-      allEmployees[department] = employeesList[department];
-    }
+    allEmployees[department] = employeesList[department];
   }
   const getNumberOfDepartments = () => Object.keys(allEmployees).length;
   return { allEmployees, getNumberOfDepartments };
