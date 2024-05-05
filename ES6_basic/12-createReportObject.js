94cfcd6dd2e73365e909ac25d24791706213866d
kvang2 <kevinvaj88@hotmail.com>
2024-05-05 16:29:53 -0500

    indenation

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,9 @@
 export default function createReportObject(employeesList) {
   const allEmployees = {};
   for (const department in employeesList) {
+    if (Object.hasOwn(employeesList, department)) {
       allEmployees[department] = employeesList[department];
+    }
   }
   const getNumberOfDepartments = () => Object.keys(allEmployees).length;
   return { allEmployees, getNumberOfDepartments };
