747763e944153466d476fd99bf7c7c22cca95b99
kvang2 <kevinvaj88@hotmail.com>
2024-05-05 16:24:37 -0500

    Created a report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+    const allEmployees = {};
+    for (const department in employeesList) {
+        allEmployees[department] = employeesList[department];
+    }
+    const getNumberOfDepartments = () => Object.keys(allEmployees).length;
+    return { allEmployees, getNumberOfDepartments };
+}
