b979a9be62a7520841b65e0ba88624fa952c747b
kvang2 <kevinvaj88@hotmail.com>
2024-05-06 16:36:46 -0500

    currency

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,14 @@
 class Currency {
     constructor(code, name) {
-      this._code = typeof code === 'string' ? code : '';
-      this._name = typeof name === 'string' ? name : '';
+        if (typeof code !== 'string' || code.trim().length === 0) {
+            throw new Error('non empty string');
+        }
+        if (typeof name !== 'string' || name.trim().length === 0) {
+            throw new Error('non empty string');
+        }
+
+      this._code = code;
+      this._name = name;
 }
 
 // Getter and setter for code
@@ -9,19 +16,21 @@ get code() {
     return this._code;
 }
 set code(newCode) {
-    if (typeof newCode === 'string') {
-        this._code = newCode;
+    if (typeof newCode === 'string' || newCode.trim().length === 0) {
+        throw new Error('non empty string');
     }
-}
+    this._code = newCode;
+  }
 
 // Getter and setter for name
 get name() {
   return this._name;
 }
 set name(newName) {
-  if (typeof newName === 'string') {
-      this._name = newName;
+  if (typeof newName === 'string' || newName.trim().length === 0) {
+    throw new Error('Name must be a non empty string');
   }
+  this._name = newName;
 }
 
 displayFullCurrency() {
