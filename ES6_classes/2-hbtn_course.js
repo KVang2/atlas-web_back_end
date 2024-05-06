bb4f4bc80ac4a1afaf91b0cf019c2f0b57f920bf
kvang2 <kevinvaj88@hotmail.com>
2024-05-06 12:52:17 -0500

    fixing file 1 indentations

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,37 +1,37 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-        this.name = typeof name === 'string' ? name : '';
-        this.length = typeof length === 'number' ? length : 0;
-        this.student = Array.isArray(students) ? students : [];
-    }
+  constructor(name, length, students) {
+      this.name = typeof name === 'string' ? name : '';
+      this.length = typeof length === 'number' ? length : 0;
+      this.student = Array.isArray(students) ? students : [];
+  }
 
 // Getter and setter for name attribute
 get name() {
-    return this._name;
+  return this._name;
 }
 set name(newName) {
-    if (typeof newName === 'string') {
-        this._name = newName;
-    }
+  if (typeof newName === 'string') {
+      this._name = newName;
+  }
 }
 
 // Getter and setter for length attri
 get length() {
-    return this._length;
+  return this._length;
 }
 set length(newLength) {
-    if (typeof newLength === 'number') {
-        this._length = newLength;
-    }
+  if (typeof newLength === 'number') {
+      this._length = newLength;
+  }
 }
 
 // Getter and setter for students attri
 get students() {
-    return this._students;
+  return this._students;
 }
 set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-        this._students = newStudents;
+  if (Array.isArray(newStudents)) {
+      this._students = newStudents;
     }
   }
 }
