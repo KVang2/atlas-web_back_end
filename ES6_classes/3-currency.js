7b299fda5fc5fee88b5261776767b1a3c9d19042
kvang2 <kevinvaj88@hotmail.com>
2024-05-06 23:07:20 -0500

    fix indentation

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,35 +1,35 @@
 export default class Currency {
-        constructor(code, name) {
-            if (typeof code !== 'string' || code.trim().length === 0) {
-            throw new Error('non empty string');
-            }
-            if (typeof name !== 'string' || name.trim().length === 0) {
-                throw new Error('non empty string');
-            }
-
-      this._code = code;
-      this._name = name;
-    }
-
-    // Getter and setter for code
-    get code() {
-        return this._code;
-    }
-    set code(newCode) {
-        if (typeof newCode !== 'string' || newCode.trim().length === 0) {
-            throw new Error('non empty string');
-        }
-        this._code = newCode;
+  constructor(code, name) {
+    if (typeof code !== 'string' || code.trim().length === 0) {
+      throw new Error('non empty string');
       }
-
-    // Getter and setter for name
-    get name() {
-      return this._name;
+    if (typeof name !== 'string' || name.trim().length === 0) {
+      throw new Error('non empty string');
     }
 
-    set name(newName) {
-      if (typeof newName !== 'string' || newName.trim().length === 0) {
-        throw new Error('Name must be a non empty string');
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and setter for code
+  get code() {
+    return this._code;
+  }
+  set code(newCode) {
+    if (typeof newCode !== 'string' || newCode.trim().length === 0) {
+      throw new Error('non empty string');
+    }
+    this._code = newCode;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string' || newName.trim().length === 0) {
+      throw new Error('Name must be a non empty string');
   }
   this._name = newName;
 }
