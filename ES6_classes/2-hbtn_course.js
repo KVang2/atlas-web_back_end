eade3d25065b0d4f2e0458139cea10360e3428ad
kvang2 <kevinvaj88@hotmail.com>
2024-05-07 11:01:00 -0500

    indentations

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,18 +1,18 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     // Verify name is a string
-      if (typeof name !== 'string' || name.trim().length === 0) {
-        throw new TypeError('Name must be non-empty string');
-      }
-
-      // Verify length is a number
-      if (typeof length !== 'number' || length <= 0) {
-        throw new TypeError('Length be positive number');
-      }
+    if (typeof name !== 'string' || name.trim().length === 0) {
+      throw new TypeError('Name must be non-empty string');
+    }
 
     // Verify length is a number
-    if (!Array.isArray(students) || students.every((student) => typeof student !== 'string' || student.trim().length === 0)) {
-        throw new TypeError('Students must be an non-empty array of strings');
+    if (typeof length !== 'number' || length <= 0) {
+      throw new TypeError('Length be positive number');
+    }
+
+    // Verify length is a number
+    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string' || student.trim().length === 0)) {
+      throw new TypeError('Students must be an non-empty array of strings');
     }
 
     // Store attributes with underscore
@@ -21,21 +21,23 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-// Getter and setter for name attribute
-get name() {
-  return this._name;
-}
-set name(newName) {
-  if (typeof newName !== 'string' || newName.trim().length === 0) {
-    throw new Error('Name must be non-empty');
+  // Getter and setter for name attribute
+  get name() {
+    return this._name;
   }
-  this._name = newName;
+
+  set name(newName) {
+    if (typeof newName !== 'string' || newName.trim().length === 0) {
+      throw new Error('Name must be non-empty');
+    }
+    this._name = newName;
+  }
+
+  // Getter and setter for length attri
+    get length() {
+    return this._length;
 }
 
-// Getter and setter for length attri
-get length() {
-  return this._length;
-}
 set length(newLength) {
   if (typeof newLength === 'number' && newLength > 0) {
       this._length = newLength;
@@ -47,7 +49,7 @@ get students() {
   return this._students;
 }
 set students(newStudents) {
-  if (!Array.isArray(newStudents) || newStudents.every(student => typeof student !== 'string' || student.trim().length === 0)) {
+  if (!Array.isArray(newStudents) || newStudents.some(student => typeof student !== 'string' || student.trim().length === 0)) {
     throw new TypeError('Students must be non-empty array of strings');
   }
   this._students = newStudents;
