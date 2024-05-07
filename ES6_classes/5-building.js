36bd5abdcfa373aae508715731f51ba93aa9bdbc
kvang2 <kevinvaj88@hotmail.com>
2024-05-07 13:34:47 -0500

    fix implement

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   // Constructor attributes
   constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== ' function') {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined || this.evacuationWarningMessage === null) {
       throw new Error('Class extending BUilding must override evacuationWarningMessage');
     }
     this._sqft = sqft;
@@ -10,10 +10,5 @@ export default class Building {
   // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // Method to be overridden by subclasses
-  evacuationWarningMessage() {
-    throw new Error('evacuationWarningMessage must be implemented by subclass');
   }
 }
