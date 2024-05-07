3f53f25e57d71489556f62282db02553b26db35f
kvang2 <kevinvaj88@hotmail.com>
2024-05-07 14:39:41 -0500

    return array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+function getListStudents() {
+    return [
+        {firstName: 'Gullaume', id: 1, location: 'San Francisco' },
+        {firstName: 'James', id: 2, location: 'Columbia' },
+        {firstName: 'Serena', id: 5, location: 'San Francisco' }
+    ];
+}
