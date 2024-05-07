fe047541c1b8f8f167606f4d676a504a34fe0bce
kvang2 <kevinvaj88@hotmail.com>
2024-05-06 23:13:48 -0500

    indentation

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -2,7 +2,7 @@ export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string' || code.trim().length === 0) {
       throw new Error('non empty string');
-      }
+    }
     if (typeof name !== 'string' || name.trim().length === 0) {
       throw new Error('non empty string');
     }
@@ -15,6 +15,7 @@ export default class Currency {
   get code() {
     return this._code;
   }
+
   set code(newCode) {
     if (typeof newCode !== 'string' || newCode.trim().length === 0) {
       throw new Error('non empty string');
@@ -28,13 +29,14 @@ export default class Currency {
   }
 
   set name(newName) {
-    if (typeof newName !== 'string' || newName.trim().length === 0) {
-      throw new Error('Name must be a non empty string');
+  if (typeof newName !== 'string' || newName.trim().length === 0) {
+    throw new Error('Name must be a non empty string');
+    }
+    this._name = newName;
   }
-  this._name = newName;
-}
 
 displayFullCurrency() {
     return(`${this._name} (${this._code})`);
+
   }
 }
