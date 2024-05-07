5d542803e8e94934765a62f0d2a28da1fc081b0c
kvang2 <kevinvaj88@hotmail.com>
2024-05-07 13:44:34 -0500

    fix type error

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   // Constructor attributes
   constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending BUilding must override evacuationWarningMessage');
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined || this.evacuationWarningMessage === null) {
+      throw Error('Class extending BUilding must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
