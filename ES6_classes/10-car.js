ae2850bafc49291a574fa89f5d9af3bd8ea6c886
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 02:15:30 -0500

    car

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,12 @@
 const cloneSymbol = Symbol('clone');
 
 export default class Car {
-    // constructor
-    constructor (brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  // constructor
+  constructor (brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+  }
 
   // Getter
   get brand() {
@@ -21,11 +21,6 @@ export default class Car {
     return this._color;
   }
 
-  [cloneSymbol]() {
-    return new Car(this._brand, this._motor, this._color);
-  }
-
   cloneCar() {
-    return this[cloneSymbol]();
-  }
+  return new this.constructor();
 }
