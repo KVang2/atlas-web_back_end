dfbf11419640ba496975cf5a882370cfd5001fa0
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 14:12:43 -0500

    test

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startstring) {
-  if ((startString === '' || typeof startString !== 'string') {
+  if (startString === '' || typeof startString !== 'string') {
     throw new Error('Invalid arguments');
   }
       const filteredValues = Array.from(set).filter(element => element.startsWith(startString));
