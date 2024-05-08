1d4681121b6b69dbf85affeb14f447441fb99b1e
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 00:59:32 -0500

    implement class HolbertonClass

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,4 +1,25 @@
 export default class HolbertonCLass {
     // Constructor attributes
-    
+    constructor(size, location) {
+        this._size = size;
+        this._location = location;
+    }
+
+    // Getter methods
+    get size() {
+        return this._size;
+    }
+
+    get location() {
+        return this._location;
+    }
+
+    // Conversion methods
+    valueOf() {
+        return this._size;
+    }
+
+    toString() {
+        return this._location;
+    }
 }
