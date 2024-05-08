fd0776c9d1f9d90eceb908dbe9a679e405eb6e0c
kvang2 <kevinvaj88@hotmail.com>
2024-05-07 20:05:02 -0500

    list

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const IdOne = {firstName: 'Guillaume', id: 1, location: 'San Francisco' };
-  const IdTwo = {firstName: 'James', id: 2, location: 'Columbia' };
-  const IdThree = {firstName: 'Serena', id: 5, location: 'San Francisco' };
+  const IdOne = { firstName: 'Guillaume', id: 1, location: 'San Francisco' };
+  const IdTwo = { firstName: 'James', id: 2, location: 'Columbia' };
+  const IdThree = { firstName: 'Serena', id: 5, location: 'San Francisco' };
 
   const arrayOfStudentsId = [IdOne, IdTwo, IdThree];
   return arrayOfStudentsId;
