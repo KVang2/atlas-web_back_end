d13a9fee1ff056dbde45fa20a25b9e069d53c69b
kvang2 <kevinvaj88@hotmail.com>
2024-05-07 20:02:31 -0500

    fix indentation

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
-    const IdOne = {firstName: 'Guillaume', id: 1, location: 'San Francisco' };
-    const IdTwo = {firstName: 'James', id: 2, location: 'Columbia' };
-    const IdThree = {firstName: 'Serena', id: 5, location: 'San Francisco' };
-    
-    const arrayOfStudentsId = [IdOne, IdTwo, IdThree];
-    return arrayOfStudentsId;
+  const IdOne = {firstName: 'Guillaume', id: 1, location: 'San Francisco' };
+  const IdTwo = {firstName: 'James', id: 2, location: 'Columbia' };
+  const IdThree = {firstName: 'Serena', id: 5, location: 'San Francisco' };
+
+  const arrayOfStudentsId = [IdOne, IdTwo, IdThree];
+  return arrayOfStudentsId;
 }
