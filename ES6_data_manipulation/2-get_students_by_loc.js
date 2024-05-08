9e860ab80a2b923aa234bf33e38a7e863f6ee49d
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 09:25:37 -0500

    passes lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
-export default function getStudentsByLocation(getListStudents, city) {
+export default function getStudentsByLocation (getListStudents, city) {
 
   if (!Array.isArray(getListStudents)) {
-   return [];   
+    return [];
   }
 
   return getListStudents().filter((student) => student.holbertonClass.location === city);
