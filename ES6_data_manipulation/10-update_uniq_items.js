45f54a2a20ddfad3dbfa40bde5b3541b2ef5992a
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 11:16:43 -0500

    test

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,10 +2,10 @@ export default function updateUniqueItems(groceryMap) {
   if (!(groceryMap instanceof Map)) {
       throw new Error('Cannot be process');
   }
-  groceryMap.forEach((val, key) => {
-      if (val === 1) {
+  groceryMap.forEach((value, key) => {
+      if (value === 1) {
           groceryMap.set(key, 100);
-      }
+    }
   });
   return groceryMap;
 }
