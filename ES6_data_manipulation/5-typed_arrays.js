b58b42e4d7eac270cb24721dd21bb891c8903cc7
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 12:29:42 -0500

    fixing typed arrray

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
     }
 
     const buffer = new ArrayBuffer(length);
-    const view = new DataView(view);
+    const view = new DataView(buffer);
     view.setInt8(position, value);
     return view;
 }
