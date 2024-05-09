d4743ba90eeb5b3bdce4eddd646651630ba325f2
kvang2 <kevinvaj88@hotmail.com>
2024-05-08 20:19:59 -0500

    return promise, parameter boolean

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI(url) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success'});
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
