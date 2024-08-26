69d12c50f9a83dce97e8a2fd84e66ec94f9e7dad
kvang2 <kevinvaj88@hotmail.com>
2024-08-26 12:48:53 -0500

    add documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""
+Script for a function use to add two floating number
+"""
 def add(a: float, b: float) -> float:
+    """
+    Adding two float numbers.
+    First float add to second float then return sums of float 1 and 2.
+    """
     return a + b
