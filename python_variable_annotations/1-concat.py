c87b6a809d7fa6348741b5b8b55dfcdf5c75fc4a
kvang2 <kevinvaj88@hotmail.com>
2024-08-27 11:47:07 -0500

    basic annotations - concat str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Script for a function use to add two floating number
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat str1, str2
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str:
+    """
+    return str1 + str2
