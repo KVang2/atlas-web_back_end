0251f5d1b5d2602a34b70852c0c6149acd2694d5
kvang2 <kevinvaj88@hotmail.com>
2024-08-27 13:10:30 -0500

    annotations to string takes float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function type annotated, takes float n as an argument and return str.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    converts float to str
+
+    Args:
+        n (float): number to convert
+    
+    Returns: str
+    """
+    return str(n)
