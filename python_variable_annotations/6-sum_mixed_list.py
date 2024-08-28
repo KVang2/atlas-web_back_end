e3b62c5494ede222edc6413b675b5f3750b2d0e3
kvang2 <kevinvaj88@hotmail.com>
2024-08-28 14:02:07 -0500

    white space

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -10,7 +10,7 @@ from typing import Union, List
 
 def sum_mixed_list(mixed_lst: List[Union[int, float]]) -> float:
     """
-    sum_mixed_list, mixed_lst, 
+    sum_mixed_list, mixed_lst,
     taking a list of integers and floats and returning the sum as a float
     Args:
     mixed_lst (List[Union[int, float]]): _description_
