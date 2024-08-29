d5a9e8183e9d6fd7ec305508d88754f0d8a6df5a
kvang2 <kevinvaj88@hotmail.com>
2024-08-28 21:41:21 -0500

    fixing pyscodestyple validation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-type-annotated function to_kv takes a string, float or int as arguments and returns tuple
+type-annotated function to_kv takes a string,
+float or int as arguments and returns tuple
 """
 
 from typing import Union, Tuple
@@ -9,12 +10,14 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    taking string, int or float and returns a tuple. second element is square of int/float v, annotate as float
+    taking string, int or float and returns a tuple.
+    second element is square of int/float v, annotate as float
     Args:
         k (str): _description_
         v (Union[int, float]): int or float
 
     Returns:
-        Tuple[str, float]: first element is string second element is square of v, annotated as float.
+        Tuple[str, float]: first element is string
+        second element is square of v, annotated as float.
     """
     return k, float(v ** 2)
