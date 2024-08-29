ab39b0e31590486452a86fd025c43cb88b1cc058
kvang2 <kevinvaj88@hotmail.com>
2024-08-28 22:33:14 -0500

    update syntax

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -21,11 +21,11 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
 
 
-tasks = [wait_random(max_delay) for _ in range(n)]
-delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
 
-for task in asyncio.as_completed(tasks):
-    delay = await task
-    delays.append(delay)
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
 
-return delays
+    return delays
