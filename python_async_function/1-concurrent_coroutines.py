3b5980870177cbcc66461fa0ad812d061cb52aaa
kvang2 <kevinvaj88@hotmail.com>
2024-08-28 22:24:30 -0500

    execute multiple coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+an asynchronous coroutine takes an int argument with
+default value and waits for a random delay and returns it
+"""
+
+
+import asyncio
+from typing import List
+import wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        List[float]: _description_
+    """
+
+
+tasks = [wait_random(max_delay) for i in range(n)]
+delays = []
+
+for task in asyncio.as_completed(tasks):
+    delay = await task
+    delays.append(delay)
+
+return delays
