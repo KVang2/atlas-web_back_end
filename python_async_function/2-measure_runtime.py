7095401aafe4778b326bed61fda21d63b1ddb7c5
kvang2 <kevinvaj88@hotmail.com>
2024-08-30 10:09:59 -0500

    fix average time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -29,6 +29,6 @@ async def measure_time(n: int, max_delay: int) -> float:
     end_time = time.time()
 
     total_time = end_time - start_time
-    aver_time = total_time / n
+    average_time = total_time / n
 
-    return aver_time
+    return average_time
