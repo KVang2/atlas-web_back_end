de02b07dfec8cedac5c656a136e20487ca5a0694
kvang2 <kevinvaj88@hotmail.com>
2024-09-04 21:39:34 -0500

    removing blank line

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -14,20 +14,20 @@ class BasicCache(BaseCaching):
         Args:
             key: where item is store
             item: value for key
-        
+
             if key or item is None, does nothing
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
-    
+
     def get(self, key):
         """
         Get value of the key
-        
+
         Args:
             key: self.cache_dat is linked to key
-        
+
         Returns:
             returns value in self.cache_data
             if key is none or doesn't exist return none.
