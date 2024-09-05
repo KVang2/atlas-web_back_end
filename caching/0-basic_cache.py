d4761b62bb38856ab898fec039803a25ce2285a2
kvang2 <kevinvaj88@hotmail.com>
2024-09-04 21:38:26 -0500

    update docstring

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
-""" Caching """
+"""Basic Caching"""
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """BasicCache inherits from BaseCaching"""
+
     def put(self, key, item):
-        """adding key-value pair"""
+        """
+        adding key-value pair
+
+        Args:
+            key: where item is store
+            item: value for key
+        
+            if key or item is None, does nothing
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
     
     def get(self, key):
-        """Get value"""
+        """
+        Get value of the key
+        
+        Args:
+            key: self.cache_dat is linked to key
+        
+        Returns:
+            returns value in self.cache_data
+            if key is none or doesn't exist return none.
+        """
         if key is None:
             return None
-        return self.cache_data.get(key, None)
-    
+        return self.cache_data.get(key)
