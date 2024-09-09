181e7b1fc5e9da9a68f78be07c5882aa1d122e7c
kvang2 <kevinvaj88@hotmail.com>
2024-09-09 12:31:46 -0500

    update MRU

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""LRU CACHING"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LURCache
+    Args:
+        BaseCaching
+    """
+
+    def __init__(self):
+        """
+        calling parent class BaseCaching 
+        """
+        super().__init__()
+        self.order = []
+    
+    def put(self, key, item):
+        """
+        Assign self.cache_data
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key is None:
+            return
+        
+        if item is None:
+            return
+        
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            MRU_key = self.order.pop()
+            del self.cache_data[MRU_key]
+            print(f'DISCARD: {MRU_key}')
+    
+        self.cache_data[key] = item
+        self.order.append(key)
+
+        if key not in self.order:
+            self.order.append(key)
+
+    def get(self, key):
+        """
+        returning value in self.cache_data linked to key
+        Args:
+            key (_type_): _description_
+        """
+        if key is None:
+            return
+        return self.cache_data.get(key)
+    
