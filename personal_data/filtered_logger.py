5fba9d7a1d85555f5cf8856aaed54562b7757bcf
kvang2 <kevinvaj88@hotmail.com>
2024-09-12 11:47:43 -0500

    fix documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,21 +8,6 @@ from typing import List
 import logging
 import re
 
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Args:
@@ -35,4 +20,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         str: _description_
     """
     pattern = '|'.join([rf'({field}=[^{separator}]+)' for field in fields])
+
     return re.sub(pattern, lambda match: f"{match.group(0).split('=')[0]}={redaction}", message)
