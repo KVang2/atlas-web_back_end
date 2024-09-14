87ac496b3c951d1da660a82d572abb2aad4d0fcd
kvang2 <kevinvaj88@hotmail.com>
2024-09-13 21:44:21 -0500

    update log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,20 +9,50 @@ import logging
 import re
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        init self, redacting formatter
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        format method to filter values in incoming log records
+        Args:
+            record (logging.LogRecord): using filter_datum values for fields in fields
+        Returns:
+            str:
+        """
+        record_message = super().format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, record_message, self.SEPARATOR)
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
     Args:
-        fields (List[str]): _description_
-        redaction (str): _description_
-        message (str): _description_
-        separator (str): _description_
+        fields (List[str]): list of strings representing all fields to obfuscate
+        redaction (str): str representing what field will be obfuscated
+        message (str): str representing log line
+        separator (str): str representing by which character is separating all fields in the log line
 
     Returns:
-        str: _description_
+        str: redacted log message
     """
+    # A regex pattern dynamically to match fields and values
     pattern = '|'.join([rf'({field}=[^{separator}]+)'
                         for field in fields])
 
+    # replace sensitive data with redaction string
     return re.sub(pattern, lambda match:
                   f"{match.group(0).split('=')[0]}={redaction}", message)
