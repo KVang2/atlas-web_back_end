c4ad4841b0920d0a57fc69e855352b066bb161d1
kvang2 <kevinvaj88@hotmail.com>
2024-09-18 12:40:10 -0500

    fixing pycodestyle validation

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,7 +15,7 @@ class BasicAuth(Auth):
     pass
 
     def extract_base64_authorization_header(
-            self, authorization_header: str) ->str:
+            self, authorization_header: str) -> str:
         """
         Method that returns Base64 part of Authorization header
         for basic authentication:
@@ -26,14 +26,14 @@ class BasicAuth(Auth):
             Base64 parts
             None: if authorization_header is None, not a string,
                 doesnt start by Basic
-                return value after basic. 
+                return value after basic.
         """
         if authorization_header is None:
             return None
-        
+
         if not isinstance(authorization_header, str):
             return None
-        
+
         if not authorization_header.startswith("Basic "):
             return None
 
