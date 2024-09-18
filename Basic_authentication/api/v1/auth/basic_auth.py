c244328d0039dba0d4cb492475144c4e59fc3a96
kvang2 <kevinvaj88@hotmail.com>
2024-09-18 16:46:37 -0500

    basic base64

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 BasicAuth inherits from Auth
 """
 
+import re
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -38,3 +41,27 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        method that returns decoded value of a Base64 str
+        Args:
+            base64_authorization_header (str):
+
+        Returns:
+            None: if base64_authorization is none
+                if is not string
+                if not a valid Base64
+        """
+        if base64_authorization_header is None:
+            return None
+        
+        if not isinstance (base64_authorization_header, str):
+            return None
+        
+        try:
+            base_auth = base64.b64decode(base64_part, validate=True)
+            return base_auth.decode('utf-8')
+        except (binascii.Error, ValueError, UnicodeDecodeError):
+            return None
