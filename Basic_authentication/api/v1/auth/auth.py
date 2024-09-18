e82e8e595a5a252d22bcc819ead36d7d524dd535
kvang2 <kevinvaj88@hotmail.com>
2024-09-18 10:37:17 -0500

    define route that doesnt need auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,28 @@ class Auth:
         Returns:
             bool: flase
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None:
+            return true
+
+        if path is excluded_paths:
+            return False
+
+        # ensure path has trailing slash
+        if path[-1] != '/':
+            path += '/'
+
+        # If path matches path in excluded paths
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """
