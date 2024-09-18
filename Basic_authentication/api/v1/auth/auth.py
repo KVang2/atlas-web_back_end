b2ba940e605b999d244ed56b341884b6096d849e
kvang2 <kevinvaj88@hotmail.com>
2024-09-17 21:01:44 -0500

    adding auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+from flask import request 
+
+
+class Auth:
+    """
+    Auth class to manage API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: list) -> bool:
+        """
+        Public method checking path requires authentication.
+        Args:
+            path (str): path to check
+            excluded_paths (list): list of path
+
+        Returns:
+            bool: flase
+        """
+        return False
+
+def authorization_header(self, request=None) -> str:
+    """
+    Public method that returns none
+    Args:
+        request (_type_, optional): Flask request object
+
+    Returns:
+        str: none
+    """
+    return None
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """
+    Publice method that returns current user, returning none for now
+    Args:
+        request: Flask request object
+    Returns:
+        User: none
+    """
+    return None
