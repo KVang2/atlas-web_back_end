04c509de2bb814aa6435a563e59699d6e57df7da
kvang2 <kevinvaj88@hotmail.com>
2024-09-18 21:42:33 -0500

    fixing pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -88,6 +88,6 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        user_e, user_pw = decoded_base64_authorization_header.split(':', 1)
 
-        return user_email, user_password
+        return user_e, user_pw
