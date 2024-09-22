3bee61f0fb075e557f267decf3df5dbc8187d5c4
kvang2 <kevinvaj88@hotmail.com>
2024-09-22 15:55:41 -0500

    add blank line for session create session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@ class session Auth inherits from Auth
 """
 
 from api.v1.auth.auth import Auth
+import uuid
+
 
 class SessionAuth(Auth):
     """
@@ -11,4 +13,30 @@ class SessionAuth(Auth):
     Args:
         Auth (_type_):
     """
-    pass
+
+    # class attribute initialized by empty dic
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creating session ID for user_id
+        Args:
+            user_id (str, optional): checking if it is None or not a string
+
+        Returns:
+            None
+        """
+        # Check if user_id is none
+        if user_id is None:
+            return
+
+        # check if user_id is not a str
+        if not isinstance(user_id, str):
+            return None
+
+        # store session Id as key and user_id as value
+        session_id = str(uuid.uuid4())
+
+        self.user_id_session_ID[session_id] = user_id
+
+        return session_id
