f854c2668cf0faef7ee285286fb27d44017334b2
kvang2 <kevinvaj88@hotmail.com>
2024-09-22 15:11:55 -0500

    update an empty sessionauth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.auth.auth import Auth
 from api.v1.views import app_views
+from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -21,6 +22,8 @@ AUTH_TYPE = os.getenv('AUTH_TYPE')
 if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+class session Auth inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """
+    Session authentication class that inherits
+    Args:
+        Auth (_type_):
+    """
+    pass
