bf671d202ad70c67e3e41f3e9090dd4869ce7adf
kvang2 <kevinvaj88@hotmail.com>
2024-09-22 15:59:01 -0500

    typo on  class attri

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -37,6 +37,6 @@ class SessionAuth(Auth):
         # store session Id as key and user_id as value
         session_id = str(uuid.uuid4())
 
-        self.user_id_session_ID[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
 
         return session_id
