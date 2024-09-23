5016d9043f8993b838eb620c21eb366aab84e1bf
kvang2 <kevinvaj88@hotmail.com>
2024-09-22 22:59:16 -0500

    fix indentation

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -50,7 +50,7 @@ def before_request():
 
     # IF AUTHORIZATION HEADER IS MISSING and session cookie, RAISE 401 ERROR
     if (auth.authorization_header(request) is None and
-        auth.session_cookie(request) is None):
+            auth.session_cookie(request) is None):
         abort(401)
 
     # assign authenticated user to request.current_user
