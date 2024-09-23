0487198fb69d0685ff9064b81e609168958df380
kvang2 <kevinvaj88@hotmail.com>
2024-09-23 10:34:53 -0500

    checking parameterized a unit test

---
+++ Unittests_and_integration_tests/README.md
@@ -0,0 +1,7 @@
+# Unittest and Intergration Tests
+
+## Learning Objectives
+- The difference between unit and integration tests is that unit tests take results from a single unit, the function call and integration tests results from various parts.
+- Mocking is a process used in unit testing when the unit is being tested has external dependencies.
+- parametrizations use for testing functions/ features that have simple input, when testing for a variety of input.
+- Fixtures ensure test runs in a clean and controlled environment, avoiding code duplications, includes setting up objects, databases, and files.
---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+parameterize a unit test, writing the first unit test for utils.access_nested_map
+"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+
+    Args:
+        unittest (_type_): _description_
+    """
+
+    @parameterized.expand([
+        "nested_map", {"a": 1}, path=("a",),
+        "nested_map2", {"a": {"b": 2}}, path=("a",),
+        "nested_map3", {"a": {"b": 2}}, path=("a", "b")
+    ])
+    def test_accesss_nested_map(self, name, nested_map, path, expected):
+        """
+        Method to test that it returns what its suppose to
+        """
+        # call access_nested_map function
+        result = access_nested_map(nested_map, path)
+
+        # check result matches expected value
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
