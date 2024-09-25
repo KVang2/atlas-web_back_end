f637434d1f2e6af319bc3288a6afcbdbb44ceff9
kvang2 <kevinvaj88@hotmail.com>
2024-09-25 17:09:45 -0500

    testing parametrize

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -92,3 +92,14 @@ class TestGithubOrgClient(unittest.TestCase):
 
             # Assert that get_json was called once with the correct URL
             mock_get_json.assert_called_once_with("https://api.github.com/orgs/google/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test has license
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
