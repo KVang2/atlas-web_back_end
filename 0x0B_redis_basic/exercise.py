32ce48400a007a572a417eb51fe58e0865ae94ac
kvang2 <kevinvaj88@hotmail.com>
2024-09-30 13:24:22 -0500

    fix port

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -16,7 +16,7 @@ class Cache:
         flush instance using flushdb
         """
         # store instance of Redis client as private client
-        self._redis = redis.Redis(host='localhost', port=6380, db=0)
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
         # flush redis database to start fresh
         self._redis.flushdb()
 
