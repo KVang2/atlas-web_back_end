4a4289a851940ba166abdd845b12ef42fbd260ef
kvang2 <kevinvaj88@hotmail.com>
2024-09-30 13:22:18 -0500

    create store method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,8 @@ Redis basic
 """
 
 import redis
+import uuid
+from typing import Union
 
 
 class Cache:
@@ -18,3 +20,11 @@ class Cache:
         # flush redis database to start fresh
         self._redis.flushdb()
 
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        takes data arg and returns string
+        generate random key using uuid
+        """
+        r_key = str(uuid.uuid4())
+        self._redis.set(r.key, data)
+        return r_key
