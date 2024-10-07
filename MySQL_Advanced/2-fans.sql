173415425ba6205f0379c9fb980eb374fcf911a1
kvang2 <kevinvaj88@hotmail.com>
2024-10-07 13:42:22 -0500

    fixing syntax

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Importing metal bands.sql.zip then ordered by number
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
