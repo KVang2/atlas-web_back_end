f2fef84ad691ae4d9c8d1423191deeaa5ef8444a
kvang2 <kevinvaj88@hotmail.com>
2024-10-07 13:36:55 -0500

    test import of table

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,18 +1,6 @@
 -- Impoprting metal bands.sql.zip then ordered by number
--- unzip metal_bands.sql.zip
-
-CREATE DATABASE IF NOT EXISTS some_database;
-Use some_database;
-
-CREATE TABLE IF NOT EXISTS metal (
-    origin VARCHAR(100),
-    nb_fans INT
-);
-
-SOURCE metal_bands.sql;
 
 SELECT origin, SUM(nb_fans) AS nb_fans,
-        RANK() OVER (ORDER BY SUM(nb_fans) DESC) AS rank
-FROM metal
+FROM metal_bands
 GROUP BY origin
-ORDER BY rank;
+ORDER BY nb_fans DESC;
