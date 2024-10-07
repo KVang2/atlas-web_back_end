e06ddcfe25b4bf0db9e110c45a78c29a18416e9a
kvang2 <kevinvaj88@hotmail.com>
2024-10-07 14:05:51 -0500

    adding better clairty

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- Ranked by longevity
 SELECT band_name,
     -- to calculate years band has been active (lifespan)
-    IFNULL(split, YEAR(CURDATE())) - formed AS lifespan
+    IFNULL(YEAR(split), YEAR(CURDATE())) - YEAR(formed) AS lifespan
 FROM metal_bands
 WHERE main_style = 'Glam rock'
 ORDER BY lifespan DESC;
