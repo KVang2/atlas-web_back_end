e46a417350a433c1e9d13cd867a25061b6431421
kvang2 <kevinvaj88@hotmail.com>
2024-10-07 14:12:04 -0500

    brands with glam rock

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,8 @@
 -- SQL script to lists all bands with Glam rock
 -- Ranked by longevity
-SELECT band_name,
-    -- to calculate years band has been active (lifespan)
-    IFNULL(YEAR(split), YEAR(CURDATE())) - YEAR(formed) AS lifespan
+SELECT
+    band_name,
+    COALESCE(split, YEAR(CURDATE())) - formed AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE main_style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
