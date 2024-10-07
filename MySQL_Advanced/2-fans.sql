47dfe754e40af724f0bff836eff8bbdef2c574d0
kvang2 <kevinvaj88@hotmail.com>
2024-10-06 20:24:00 -0500

    importing table and  order rank

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,18 @@
+-- Impoprting metal bands.sql.zip then ordered by number
+-- unzip metal_bands.sql.zip
+
+CREATE DATABASE IF NOT EXISTS some_database;
+Use some_database;
+
+CREATE TABLE IF NOT EXISTS metal (
+    origin VARCHAR(100),
+    nb_fans INT
+);
+
+SOURCE metal_bands.sql;
+
+SELECT origin, SUM(nb_fans) AS non_unique_fans,
+        RANK() OVER (ORDER BY SUM(nb_fans) DESC) AS rank
+FROM metal
+GROUP BY origin
+ORDER BY rank;
