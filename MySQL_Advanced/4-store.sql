da21b215524e990eee35d648c22f5709e6556873
kvang2 <kevinvaj88@hotmail.com>
2024-10-07 20:36:54 -0500

    add delimter

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,8 +1,13 @@
 -- SQL script that creates a trigger that decreases the quantity of an item
+-- Add delimiter
+DELIMITER //
+
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW 
 BEGIN 
     UPDATE items SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END;
+END //
+
+DELIMITER ;
