9811a634d20dc71022b3e8a2e2196638c4ddd710
kvang2 <kevinvaj88@hotmail.com>
2024-10-08 09:14:28 -0500

    spelling check

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER reset_email
 AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email then
+    IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END //
