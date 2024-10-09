746df1edf814bfa1084a501af2e0c86af39e5cb4
kvang2 <kevinvaj88@hotmail.com>
2024-10-09 11:46:59 -0500

    table for meeting

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- create view for meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM last_meeting
+WHERE score < 80;
+    AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
