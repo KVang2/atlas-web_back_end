cecf83f3074c1498d42f36672da567805fe7a471
kvang2 <kevinvaj88@hotmail.com>
2024-10-09 12:10:51 -0500

    list all documents inpython

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 """ list all documents in python """
 import pymongo
+
+def list_all(mongo_collection):
+    """ Lists all documents """
+    if mongo_collection.count_documents({}) == 0:
+        return []
+
+    return list(mongo_collection.find())
