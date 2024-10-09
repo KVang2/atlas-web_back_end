039a0e5810683df2797aec37136b427d955823e0
kvang2 <kevinvaj88@hotmail.com>
2024-10-09 11:50:24 -0500

    update code

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,6 @@
 -- create view for meeting
 CREATE VIEW need_meeting AS
 SELECT name
-FROM last_meeting
+FROM student
 WHERE score < 80
     AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
