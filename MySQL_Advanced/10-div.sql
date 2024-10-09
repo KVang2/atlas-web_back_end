6cb9927457f059fbf06cef653c8f37ef3bb2a4a3
kvang2 <kevinvaj88@hotmail.com>
2024-10-09 10:50:32 -0500

    division for sql adding condition in function

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- creates function safeDiv (divides and return)
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+
+    ELSE
+    RETURNS a / b;
+    END if;
+END;
