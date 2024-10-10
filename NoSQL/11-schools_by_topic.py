dfa8e212286ea109c7e866b00e047ae168472cdf
kvang2 <kevinvaj88@hotmail.com>
2024-10-09 19:38:17 -0500

    update school topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -3,4 +3,8 @@
 from pymongo import MongoClient
 
 def schools_by_topic(mongo_collection, topic):
-    
+    """
+    mongo_collection, topic
+    Returns: list of specific topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+""" provides stats about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
