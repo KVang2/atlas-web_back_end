bd1e35a1eeff8d3f6a2fc747a60255265d2803ea
kvang2 <kevinvaj88@hotmail.com>
2024-10-13 22:17:38 -0500

    task 0 executing basic javascript with Node JS

--- Node_JS/0-console.js
+++ Node_JS/0-console.js
@@ -1,4 +1,6 @@
 // Executing basic Javascript with Node JS
-export function displayMessage(message) {
+function displayMessage(message) {
     console.log(message);
 }
+
+module.exports = displayMessage
