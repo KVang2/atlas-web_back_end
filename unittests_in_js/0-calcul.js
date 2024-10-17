93d1af82ba6e06a62eee6fa96133b252da6f4240
kvang2 <kevinvaj88@hotmail.com>
2024-10-17 11:20:22 -0500

    test file

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+// creating function
+const assert = require('assert;');
+
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+}
---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,9 @@
+import unittest
+
+class calculateNumber(unittest.TestCase);
+
+    def calculatePositiveNumber(self);
+        self.assertEqual(add_and_round(2.7, 3.9, 7)
+
+if __name__ = 'main';
+    unittest.main()
